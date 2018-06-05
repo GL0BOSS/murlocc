1e44aa2eacfdd1b4b1d11482f3d4b00e96a0b894
GL0BOSS <39969601+GL0BOSS@users.noreply.github.com>
2018-06-05 22:50:20 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+	ClientUser.setActivity("Murloc !help");
+	console.log("Mrglglglgl");
+});
+
+bot.login("NDQ4OTMzODk3NDk4MTMyNDgw.Dfh1Kg.6DC-5WUl6MIHves21IGqV5l-lzs");
